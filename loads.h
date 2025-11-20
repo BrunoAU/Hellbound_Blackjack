11a53837a5bd5673c7031ca61e32f2dac3546bcc
Bruno Augusto <bruno.filho1000@gmail.com>
2025-11-20 14:37:51 -0300

    Create loads.h

---
+++ loads.h
@@ -0,0 +1,99 @@
+#ifndef LOADS_H
+#define LOADS_H
+
+void load_cursor(void);
+void unload_cursor(void);
+
+void load_bolsa(void);
+void unload_bolsa(void);
+
+void load_coracoes(void);
+void unload_coracoes(void);
+
+void load_baralho(void);
+void unload_baralho(void);
+
+void load_fundo(void);
+void unload_fundo(void);
+
+void load_carta_a(void);
+void unload_carta_a(void);
+
+void load_carta_2(void);
+void unload_carta_2(void);
+
+void load_carta_3(void);
+void unload_carta_3(void);
+
+void load_carta_4(void);
+void unload_carta_4(void);
+
+void load_carta_5(void);
+void unload_carta_5(void);
+
+void load_carta_6(void);
+void unload_carta_6(void);
+
+void load_carta_7(void);
+void unload_carta_7(void);
+
+void load_carta_8(void);
+void unload_carta_8(void);
+
+void load_carta_9(void);
+void unload_carta_9(void);
+
+void load_carta_10(void);
+void unload_carta_10(void);
+
+void load_carta_j(void);
+void unload_carta_j(void);
+
+void load_carta_q(void);
+void unload_carta_q(void);
+
+void load_carta_k(void);
+void unload_carta_k(void);
+
+void load_carta_joker(void);
+void unload_carta_joker(void);
+
+void load_todas_cartas(void);
+void unload_todas_cartas(void);
+
+void load_inventario(void);
+void unload_inventario(void);
+
+void unload_vitoria(void);
+void load_vitoria(void);
+
+void unload_derrota(void);
+void load_derrota(void);
+
+void load_continuar(void);
+void unload_continuar(void);
+
+void load_stand(void);
+void unload_stand(void);
+
+void load_empate(void);
+void unload_empate(void);
+
+void load_vitoria21(void);
+void unload_vitoria21(void);
+
+void load_trinket_mais4(void);
+void unload_trinket_mais4(void);
+void load_trinket_menos4(void);
+void unload_trinket_menos4(void);
+void load_trinket_mais2(void);
+void unload_trinket_mais2(void);
+void load_trinket_menos2(void);
+void unload_trinket_menos2(void);
+void load_trinkets_inventario(void);
+void unload_trinkets_inventario(void);
+
+void load_tudo(void);
+void unload_tudo(void);
+
+#endif
